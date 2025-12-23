f0aaf4da0411a238af434485988a08f3bd62161c
ahmedabelrahman-dev <rman79340@gmail.com>
2025-12-23 17:23:53 +0400

    feat: setup register with Clerk/next.js

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
-import { Button } from '@/components/ui/button';
+import { SignedIn, SignedOut, SignUpButton, UserButton } from '@clerk/nextjs';
 
 export default function Home() {
   return (
-    <div>
-      <Button>Click me</Button>
+    <div className="m-5">
+      <SignedOut>
+        <SignUpButton>
+          <button className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+            Sign Up
+          </button>
+        </SignUpButton>
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
     </div>
   );
 }
